d1591eca43eda9f4f57e149655cc478c20f066bb
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-07-16 18:38:39 +0100

    Add user purse and user purse transaction entities

--- src/app/Domain/UserPurse/Entity/UserPurse.php
+++ src/app/Domain/UserPurse/Entity/UserPurse.php
@@ -1,15 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joesweeny
- * Date: 16/07/18
- * Time: 18:13
- */
 
 namespace BackToWin\Domain\UserPurse\Entity;
 
+use BackToWin\Framework\Entity\PrivateAttributesTrait;
+use BackToWin\Framework\Entity\TimestampedTrait;
+use BackToWin\Framework\Uuid\Uuid;
+use Money\Money;
 
 class UserPurse
 {
+    use PrivateAttributesTrait,
+        TimestampedTrait;
 
+    public function setUserId(Uuid $userId): self
+    {
+        return $this->set('user_id', $userId);
+    }
+
+    public function getUserId(): Uuid
+    {
+        return $this->getOrFail('user_id');
+    }
+
+    public function setTotal(Money $money): self
+    {
+        return $this->set('total', $money);
+    }
+
+    public function getTotal(): Money
+    {
+        return $this->getOrFail('total');
+    }
 }
--- src/app/Domain/UserPurse/Entity/UserPurseTransaction.php
+++ src/app/Domain/UserPurse/Entity/UserPurseTransaction.php
@@ -1,15 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joesweeny
- * Date: 16/07/18
- * Time: 18:21
- */
 
 namespace BackToWin\Domain\UserPurse\Entity;
 
+use BackToWin\Framework\Calculation\Calculation;
+use BackToWin\Framework\Entity\PrivateAttributesTrait;
+use BackToWin\Framework\Entity\TimestampedTrait;
+use BackToWin\Framework\Identity\IdentifiedByUuidTrait;
+use BackToWin\Framework\Uuid\Uuid;
+use Money\Money;
 
 class UserPurseTransaction
 {
+    use PrivateAttributesTrait,
+        IdentifiedByUuidTrait,
+        TimestampedTrait;
 
+    public function setUserId(Uuid $userId): self
+    {
+        return $this->set('user_id', $userId);
+    }
+
+    public function getUserId(): Uuid
+    {
+        return $this->getOrFail('user_id');
+    }
+
+    public function setTotal(Money $money): self
+    {
+        return $this->set('total', $money);
+    }
+
+    public function getTotal(): Money
+    {
+        return $this->getOrFail('total');
+    }
+
+    public function setCalculation(Calculation $calculation): self
+    {
+        return $this->get('calculation', $calculation);
+    }
+
+    public function getCalculation(): Calculation
+    {
+        return $this->getOrFail('calculation');
+    }
+
+    public function setDescription(string $description): self
+    {
+        return $this->set('description', $description);
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->get('description');
+    }
 }

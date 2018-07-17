87ebe0d8f5f5d0c24d00d05203489702e43de17f
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-07-17 22:02:28 +0100

    Add GameType enum

---
+++ src/app/Domain/Game/Enum/GameType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BackToWin\Domain\Game\Enum;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static GameType WINNER_TAKES_ALL()
+ */
+class GameType extends Enum
+{
+    const WINNER_TAKES_ALL = 'WINNER_TAKES_ALL';
+}

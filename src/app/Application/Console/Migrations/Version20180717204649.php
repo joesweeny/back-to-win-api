2cc576fdd9a8c799a896f553b44cb9416854218c
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-07-17 21:57:44 +0100

    Add migration for game table

---
+++ src/app/Application/Console/Migrations/Version20180717204649.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BackToWin\Application\Console\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180717204649 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('game');
+        $table->addColumn('id', Type::BINARY)->setLength(16);
+        $table->addColumn('type', Type::STRING);
+        $table->addColumn('currency', Type::STRING);
+        $table->addColumn('max', Type::INTEGER);
+        $table->addColumn('min', Type::INTEGER);
+        $table->addColumn('status', Type::STRING);
+        $table->addColumn('created_at', Type::INTEGER);
+        $table->addColumn('updated_at', Type::INTEGER);
+        $table->setPrimaryKey(['id']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('game');
+    }
+}

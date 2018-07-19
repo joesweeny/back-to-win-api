d8c8ea7023a28e1d628822d0a03b071195d45edf
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-07-19 21:25:04 +0100

    Add game query builder

---
+++ src/app/Domain/Game/Persistence/Illuminate/GameQueryBuilder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BackToWin\Domain\Game\Persistence\Illuminate;
+
+use BackToWin\Domain\Game\Persistence\GameRepositoryQuery;
+use Illuminate\Database\Query\Builder;
+
+class GameQueryBuilder
+{
+    public function build(Builder $builder, GameRepositoryQuery $query = null): Builder
+    {
+        $query = $query ?: new GameRepositoryQuery();
+
+        if (($id = $query->getWhereIdEquals()) !== null) {
+            $builder->where('id', $id->toBinary());
+        }
+
+        if (($status = $query->getWhereStatusEquals()) !== null) {
+            $builder->where('status', $status->getValue());
+        }
+
+        if (($type = $query->getWhereTypeEquals()) !== null) {
+            $builder->where('type', $type->getValue());
+        }
+
+        return $builder;
+    }
+}

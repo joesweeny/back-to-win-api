d0cca7875840e888d60474975a34f652aaebcdcc
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-07-23 21:50:11 +0100

    Add illuminate implementation, fixed clock and hydrator

---
+++ src/app/Domain/GameEntry/Persistence/Hydration/Hydrator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BackToWin\Domain\GameEntry\Persistence\Hydration;
+
+use BackToWin\Domain\GameEntry\Entity\GameEntry;
+use BackToWin\Framework\Uuid\Uuid;
+
+class Hydrator
+{
+    /**
+     * Hydrate a GameEntry object from scalar object
+     *
+     * @param \stdClass $data
+     * @return GameEntry
+     */
+    public static function fromRawData(\stdClass $data): GameEntry
+    {
+        return new GameEntry(
+            $data->id,
+            Uuid::createFromBinary($data->game_id),
+            Uuid::createFromBinary($data->user_id)
+        );
+    }
+}

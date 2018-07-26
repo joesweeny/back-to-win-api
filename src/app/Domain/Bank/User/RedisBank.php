3558bca210cb4f58cfbfdf75980560b06440028a
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-07-26 22:34:05 +0100

    Pull in predis package

---
+++ src/app/Domain/Bank/User/RedisBank.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joesweeny
+ * Date: 26/07/18
+ * Time: 21:30
+ */
+
+namespace BackToWin\Domain\Bank\User;
+
+
+class RedisBank
+{
+
+}
---
+++ src/app/Testing/Traits/UsesTestRedisDatabase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joesweeny
+ * Date: 26/07/18
+ * Time: 22:16
+ */
+
+namespace BackToWin\Testing\Traits;
+
+
+trait UsesTestRedisDatabase
+{
+
+}

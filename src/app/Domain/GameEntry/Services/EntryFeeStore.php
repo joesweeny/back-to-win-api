bb36002334e2929c3e2f60cc4f23921bc7183db8
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-13 18:08:34 +0100

    Refactor entry fee store interface

--- src/app/Domain/GameEntry/Services/EntryFeeStore.php
+++ src/app/Domain/GameEntry/Services/EntryFeeStore.php
@@ -2,7 +2,6 @@
 
 namespace BackToWin\Domain\GameEntry\Services;
 
-use BackToWin\Domain\Game\Entity\Game;
 use BackToWin\Domain\GameEntry\Entity\GameEntry;
 use BackToWin\Domain\GameEntry\Exception\EntryFeeStoreException;
 use BackToWin\Framework\Uuid\Uuid;
@@ -11,13 +10,8 @@ use Money\Money;
 interface EntryFeeStore
 {
     /**
-     * @param Game $game
-     * @throws  EntryFeeStoreException
-     * @return void
-     */
-    public function create(Game $game): void;
-
-    /**
+     * Sort entry fee against GameEntry i.e. Game ID and User ID
+     *
      * @param GameEntry $entry
      * @param Money $fee
      * @throws EntryFeeStoreException
@@ -25,18 +19,10 @@ interface EntryFeeStore
     public function enter(GameEntry $entry, Money $fee): void;
 
     /**
-     * Return the winning value for Game
+     * Return the total value for Game
      *
      * @param Uuid $gameId
      * @return Money
      */
     public function getFeeTotal(Uuid $gameId): Money;
-
-    /**
-     * Delete Game record from store
-     *
-     * @param Uuid $gameId
-     * @throws EntryFeeStoreException
-     */
-    public function delete(Uuid $gameId): void;
 }

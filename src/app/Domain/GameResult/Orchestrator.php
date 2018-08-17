82c672f152c10bdd5d5a20cabea795807a3c3853
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-17 16:37:55 +0100

    Add orchestrator

---
+++ src/app/Domain/GameResult/Orchestrator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BackToWin\Domain\GameResult;
+
+use BackToWin\Domain\GameResult\Persistence\Repository;
+use BackToWin\Framework\Exception\RepositoryDuplicationException;
+use BackToWin\Framework\Uuid\Uuid;
+
+class Orchestrator
+{
+    /**
+     * @var Repository
+     */
+    private $repository;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param Uuid $gameId
+     * @param Uuid $winnerId
+     * @throws RepositoryDuplicationException
+     * @return void
+     */
+    public function saveGameWinner(Uuid $gameId, Uuid $winnerId): void
+    {
+        $this->repository->insert($gameId, $winnerId);
+    }
+}

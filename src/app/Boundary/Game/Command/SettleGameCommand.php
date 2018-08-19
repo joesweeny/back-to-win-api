824f6190b0756c29c261fa73de88832caabc286d
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-19 21:11:09 +0100

    Settle game command

--- src/app/Boundary/Game/Command/SettleGameCommand.php
+++ src/app/Boundary/Game/Command/SettleGameCommand.php
@@ -1,15 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joesweeny
- * Date: 19/08/18
- * Time: 20:56
- */
 
 namespace BackToWin\Boundary\Game\Command;
 
+use BackToWin\Framework\Uuid\Uuid;
+use Chief\Command;
+use Money\Currency;
+use Money\Money;
 
-class SettleGameCommand
+class SettleGameCommand implements Command
 {
+    /**
+     * @var Uuid
+     */
+    private $gameId;
+    /**
+     * @var Uuid
+     */
+    private $userId;
+    /**
+     * @var string
+     */
+    private $currency;
+    /**
+     * @var int
+     */
+    private $total;
 
+    public function __construct(string $gameId, string $userId, string $currency, int $total)
+    {
+        $this->gameId = new Uuid($gameId);
+        $this->userId = new Uuid($userId);
+        $this->currency = $currency;
+        $this->total = $total;
+    }
+
+    public function getGameId(): Uuid
+    {
+        return $this->gameId;
+    }
+
+    public function getUserId(): Uuid
+    {
+        return $this->userId;
+    }
+
+    public function getMoney(): Money
+    {
+        return new Money($this->total, new Currency($this->currency));
+    }
 }

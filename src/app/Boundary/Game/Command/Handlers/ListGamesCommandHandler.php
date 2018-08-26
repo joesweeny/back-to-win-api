6ae0d8496847239b08bc810bfead14f826634f3d
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-26 21:11:01 +0100

    Handle query paramaters in List Games command and handler

--- src/app/Boundary/Game/Command/Handlers/ListGamesCommandHandler.php
+++ src/app/Boundary/Game/Command/Handlers/ListGamesCommandHandler.php
@@ -4,6 +4,7 @@ namespace GamePlatform\Boundary\Game\Command\Handlers;
 
 use GamePlatform\Boundary\Game\Command\ListGamesCommand;
 use GamePlatform\Boundary\Game\GamePresenter;
+use GamePlatform\Boundary\Game\QueryBuilder;
 use GamePlatform\Domain\Game\Entity\Game;
 use GamePlatform\Domain\Game\GameOrchestrator;
 
@@ -17,17 +18,30 @@ class ListGamesCommandHandler
      * @var GamePresenter
      */
     private $presenter;
+    /**
+     * @var QueryBuilder
+     */
+    private $builder;
 
-    public function __construct(GameOrchestrator $orchestrator, GamePresenter $presenter)
+    public function __construct(GameOrchestrator $orchestrator, GamePresenter $presenter, QueryBuilder $builder)
     {
         $this->orchestrator = $orchestrator;
         $this->presenter = $presenter;
+        $this->builder = $builder;
     }
 
+    /**
+     * @param ListGamesCommand $command
+     * @throws \UnexpectedValueException
+     * @throws \InvalidArgumentException
+     * @return array
+     */
     public function handle(ListGamesCommand $command): array
     {
+        $query = $this->builder->buildGameRepositoryQuery($command->getQueryParameters());
+
         return array_map(function (Game $game) {
             return $this->presenter->toDto($game);
-        }, $this->orchestrator->getGames());
+        }, $this->orchestrator->getGames($query));
     }
 }

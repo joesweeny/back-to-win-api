e414f480273dee089f4eef3e947cf18a205a19a9
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-27 19:34:18 +0100

    Handle not found exceptions in json error response factory

--- src/app/Framework/Middleware/Error/JsonErrorResponseFactory.php
+++ src/app/Framework/Middleware/Error/JsonErrorResponseFactory.php
@@ -2,7 +2,10 @@
 
 namespace GamePlatform\Framework\Middleware\Error;
 
+use GamePlatform\Framework\Exception\NotFoundException;
+use GamePlatform\Framework\Jsend\JsendError;
 use GamePlatform\Framework\Jsend\JsendErrorResponse;
+use GamePlatform\Framework\Jsend\JsendFailResponse;
 use Psr\Http\Message\ResponseInterface;
 
 class JsonErrorResponseFactory implements ErrorResponseFactory
@@ -12,6 +15,12 @@ class JsonErrorResponseFactory implements ErrorResponseFactory
      */
     public function create(\Throwable $exception): ResponseInterface
     {
+        if ($exception instanceof NotFoundException) {
+            return (new JsendFailResponse([
+                new JsendError($exception->getMessage() ?: 'Not Found', 403)
+            ]))->withStatus(404);
+        }
+
         return new JsendErrorResponse();
     }
 }

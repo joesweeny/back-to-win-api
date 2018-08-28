0511f9790284294ebdf9fab7dfd175bc79984e93
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-08-28 21:46:51 +0100

    Implement JWT generator generate method and add tests

--- src/app/Domain/Auth/Services/Token/Jwt/JwtGenerator.php
+++ src/app/Domain/Auth/Services/Token/Jwt/JwtGenerator.php
@@ -2,16 +2,47 @@
 
 namespace GamePlatform\Domain\Auth\Services\Token\Jwt;
 
+use GamePlatform\Bootstrap\Config;
 use GamePlatform\Domain\Auth\Services\Token\Generator;
+use GamePlatform\Framework\DateTime\Clock;
 use GamePlatform\Framework\Uuid\Uuid;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 
 class JwtGenerator implements Generator
 {
+    /**
+     * @var Config
+     */
+    private $config;
+    /**
+     * @var Clock
+     */
+    private $clock;
+    /**
+     * @var Sha256
+     */
+    private $signer;
+
+    public function __construct(Config $config, Clock $clock, Sha256 $signer)
+    {
+        $this->config = $config;
+        $this->clock = $clock;
+        $this->signer = $signer;
+    }
+
     /**
      * @inheritdoc
      */
     public function generate(Uuid $userId, \DateTimeImmutable $expiry): string
     {
-        // TODO: Implement generate() method.
+        $token = (new Builder())
+            ->setIssuedAt($this->clock->now()->getTimestamp())
+            ->setExpiration($expiry->getTimestamp())
+            ->set('user_id', (string) $userId)
+            ->sign($this->signer, $this->config->get('auth.jwt.secret'))
+            ->getToken();
+
+        return $token->__toString();
     }
 }
--- tests/Domain/Auth/Services/Token/Jwt/JwtGeneratorTest.php
+++ tests/Domain/Auth/Services/Token/Jwt/JwtGeneratorTest.php
@@ -1,15 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: joesweeny
- * Date: 28/08/18
- * Time: 21:26
- */
 
 namespace GamePlatform\Domain\Auth\Services\Token\Jwt;
 
+use GamePlatform\Bootstrap\Config;
+use GamePlatform\Domain\Auth\Services\Token\Generator;
+use GamePlatform\Framework\Uuid\Uuid;
+use GamePlatform\Testing\Traits\UsesContainer;
+use Interop\Container\ContainerInterface;
+use Lcobucci\JWT\Parser;
+use PHPUnit\Framework\TestCase;
 
-class JwtGeneratorTest
+class JwtGeneratorTest extends TestCase
 {
+    use UsesContainer;
 
+    /** @var  ContainerInterface */
+    private $container;
+    /** @var  Generator */
+    private $generator;
+    /** @var  Config */
+    private $config;
+
+    public function setUp()
+    {
+        $this->container = $this->createContainer();
+        $this->config = $this->container->get(Config::class);
+        $this->config->set('auth.token.driver', 'jwt');
+        $this->generator = $this->container->get(Generator::class);
+    }
+
+    public function test_interface_is_bound()
+    {
+        $this->assertInstanceOf(Generator::class, $this->generator);
+    }
+
+    public function test_generate_returns_a_valid_encoded_jwt_token_string()
+    {
+        $token = $this->generator->generate(
+            new Uuid('058b1dc8-d168-4bab-aa4a-ffeed90c5435'),
+            (new \DateTimeImmutable())->add(new \DateInterval('P1D'))
+        );
+
+        $parsed = (new Parser())->parse($token);
+
+        $this->assertEquals('058b1dc8-d168-4bab-aa4a-ffeed90c5435', $parsed->getClaim('user_id'));
+        $this->assertFalse($parsed->isExpired());
+        $this->assertEquals('JWT', $parsed->getHeader('typ'));
+        $this->assertEquals('HS256', $parsed->getHeader('alg'));
+    }
 }

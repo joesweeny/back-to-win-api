7eae8f98c74b25a48bea526a3b63c606a4c34cfb
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-09-16 15:50:43 +0100

    Restart nginx when pushing changes to staging

--- .circleci/staging/docker-deploy.sh
+++ .circleci/staging/docker-deploy.sh
@@ -3,4 +3,6 @@
 set -e
 
 scp docker-compose.staging.yml root@${STAGING_SERVER}:~/staging
-ssh root@${STAGING_SERVER} "docker-compose -f ./staging/docker-compose.staging.yml pull web && docker-compose -f ./staging/docker-compose.staging.yml up -d"
+ssh root@${STAGING_SERVER} "docker-compose -f ./staging/docker-compose.staging.yml pull web && \
+docker-compose -f ./staging/docker-compose.staging.yml up -d && \
+docker-compose -f ./nginx-proxy/docker-compose.staging.yml up -d"

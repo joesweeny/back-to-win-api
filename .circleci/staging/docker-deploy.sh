4378fef2da382db91a2f9362e72c9ccde60c030c
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-09-18 21:57:14 +0100

    Reload nginx when deploying to master

--- .circleci/staging/docker-deploy.sh
+++ .circleci/staging/docker-deploy.sh
@@ -5,4 +5,4 @@ set -e
 scp docker-compose.staging.yml root@${STAGING_SERVER}:~/staging
 ssh root@${STAGING_SERVER} "docker-compose -f ./staging/docker-compose.staging.yml pull web && \
 docker-compose -f ./staging/docker-compose.staging.yml up -d && \
-docker-compose -f ./nginx-proxy/docker-compose.staging.yml up -d"
+docker restart nginxproxy_nginx-proxy_1"

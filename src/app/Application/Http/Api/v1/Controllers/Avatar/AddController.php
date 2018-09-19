8b598b354309c9679a46047102d47e6601bf5da4
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-09-19 21:21:23 +0100

    Add avatar request validator

---
+++ src/app/Application/Http/Api/v1/Controllers/Avatar/AddController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joesweeny
+ * Date: 19/09/18
+ * Time: 21:18
+ */
+
+namespace GamePlatform\Application\Http\Api\v1\Avatar;
+
+
+class AddController
+{
+
+}
